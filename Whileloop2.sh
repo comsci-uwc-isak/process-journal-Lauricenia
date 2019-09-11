1b4d7579c1063f07fc60d4819eb33746a6cb9605
M19-012 <m19-012@M19-012s.local>
2019-09-11 16:33:10 +0900

    While loop new comands: lesson 5

---
+++ Whileloop2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This program demonstrates the use of while loops
+
+i=0
+
+while [ $i -le 5 ]
+do
+	if [ $i -eq 6 ]; then 
+		echo "loop has been terminated"
+		break
+	fi
+
+       (( i++ ))
+	if [ $i -eq 3 ]; then 
+		echo "skipping"
+		continue 
+		#this will skip iteration 3
+	fi
+        echo $i
+       
+done
+
